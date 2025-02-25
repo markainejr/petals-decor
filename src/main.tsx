b13d9978511e8d12fbd9a0e9bfd32b0bba204013
markainejr <110311915+markainejr@users.noreply.github.com>
2025-02-25 16:05:05 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx';
 import './index.css';
 
 // Initialize Google Analytics
-ReactGA.initialize('G-XXXXXXXXXX'); // Replace with your Google Analytics measurement ID
+ReactGA.initialize('G-50XR2C2KY4'); // Replace with your Google Analytics measurement ID
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
